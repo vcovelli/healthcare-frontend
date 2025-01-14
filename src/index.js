4eb3edd73f625f38874ef1f092b2d3eb84da96bd
vcovelli <vincent.covelli78@gmail.com>
2025-01-14 14:43:23 -0500

    Import Tailwind output.css in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Updated import for React 18
+import './output.css'; // Use the generated output file
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
