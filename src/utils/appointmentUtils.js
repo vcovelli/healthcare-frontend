8b81b125aad982b4e6b348b98008f83e84a1acad
vcovelli <182012017+vcovelli@users.noreply.github.com>
2025-01-21 12:20:39 -0500

    Updated API integration and utility functions for appointments and role handling

---
+++ src/utils/appointmentUtils.js
@@ -0,0 +1,11 @@
+export const isTimeAvailable = (appointments, dateTime) => {
+    if (!dateTime) return false;
+    const selectedDate = dateTime.toISOString().split("T")[0]; // Get the selected date (YYYY-MM-DD)
+    const formattedTime = dateTime.toTimeString().split(" ")[0]; // Get the time in HH:MM:SS format
+  
+    return !appointments.some(
+      (appointment) =>
+        appointment.date === selectedDate && appointment.time === formattedTime
+    );
+  };
+  
