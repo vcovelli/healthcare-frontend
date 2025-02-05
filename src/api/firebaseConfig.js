bb3968b8286c232ada12205b401e86ae35ce833f
vcovelli <182012017+vcovelli@users.noreply.github.com>
2025-02-05 11:16:39 -0500

    Cleaned up and restructured code.

--- src/api/firebaseConfig.js
+++ src/api/firebaseConfig.js
@@ -3,6 +3,24 @@ import { initializeApp } from "firebase/app";
 import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth";
 import apiClient from "./apiClient";
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Set auth persistence to persist across reloads
+setPersistence(auth, browserLocalPersistence)
+  .catch((error) => console.error("Error setting persistence:", error));
+
 export const getUserRole = async () => {
   try {
     const currentUser = auth.currentUser;
@@ -27,26 +45,6 @@ export const getUserRole = async () => {
     return null;
   }
 };
-
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-// Set auth persistence to persist across reloads
-setPersistence(auth, browserLocalPersistence)
-  .catch((error) => console.error("Error setting persistence:", error));
 
 console.log("API Key:", process.env.REACT_APP_FIREBASE_API_KEY);
 console.log("Auth Domain:", process.env.REACT_APP_FIREBASE_AUTH_DOMAIN);
